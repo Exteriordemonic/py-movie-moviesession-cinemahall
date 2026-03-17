db9d66164a025a631e87604f9ccf30401c337703
Kamil <kmirosz6@gmail.com>
2026-03-17 22:53:31 +0100

    fix issues

--- services/movie_session.py
+++ services/movie_session.py
@@ -13,7 +13,7 @@ def create_movie_session(
 
 
 def get_movies_sessions(
-    session_date: datetime.date = None,
+    session_date: str = None,
 ) -> list[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
